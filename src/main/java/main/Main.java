c3ee271cbb02e07a20f448a2bc079a9645a8b784
vladnau89 <vladnau89@yandex.ru>
2015-12-14 23:04:39 +0300

    add mirror request

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,12 +1,28 @@
 package main;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import servlets.MirrorRequestServlet;
+
 /**
  * Created by win on 14.12.2015.
  */
 public class Main {
 
     public static void main(String[] atgs) throws Exception{
+        MirrorRequestServlet mirrorRequestServlet = new MirrorRequestServlet();
 
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(mirrorRequestServlet), "/mirror");
+
+        Server server = new Server(8080);
+        server.setHandler(context);
+        server.start();
+
+        System.out.println("Server started");
+
+        server.join();
     }
 
 }
---
+++ src/main/java/servlets/MirrorRequestServlet.java
@@ -0,0 +1,24 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by win on 14.12.2015.
+ */
+public class MirrorRequestServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        String value = request.getParameterValues("key")[0];
+        response.getWriter().print(value);
+    }
+}
