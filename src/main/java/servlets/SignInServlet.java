3ab91678cbea488da9bbbef6ea75e03a16c83b84
vladnau89 <vladnau89@yandex.ru>
2015-12-23 00:12:37 +0300

    complete SignInServlet

--- src/main/java/servlets/SignInServlet.java
+++ src/main/java/servlets/SignInServlet.java
@@ -1,11 +1,13 @@
 package servlets;
 
 import accounts.AccountService;
+import accounts.UserProfile;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by win on 22.12.2015.
@@ -19,8 +21,28 @@ public class SignInServlet extends HttpServlet {
     }
 
     public void doPost( HttpServletRequest request, HttpServletResponse response )
-            throws ServletException
+            throws ServletException, IOException
     {
+        String login = request.getParameter("login");
+        String pass  = request.getParameter("password");
+
+        response.setContentType("text/html;charset=utf-8");
+
+        if (login == null || pass == null){
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().print("Unauthorized");
+            return;
+        }
+
+        UserProfile profile = accountService.getUserByLogin(login);
+        if (profile == null || !profile.getPass().equals(pass)){
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().print("Unauthorized");
+        }
+        else{
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.getWriter().print("Authorized");
+        }
 
     }
 
